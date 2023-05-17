21a97a22c4f35d7d926107c07250d5678bb1fdce
PHoMi126 <caidinhmenh2612@gmail.com>
2023-05-17 18:45:23 +0700

    joystick

--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Rigidbody _rb;
     [SerializeField] private FixedJoystick _joystick;
     [SerializeField] private Animator _animator;
     [SerializeField] private float _speed;
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector3(_joystick.Horizontal * _speed, _rigidbody.velocity.y, _joystick.Vertical * _speed);
+        _rb.velocity = new Vector3(_joystick.Horizontal * _speed, _rb.velocity.y, _joystick.Vertical * _speed);
     }
 }
