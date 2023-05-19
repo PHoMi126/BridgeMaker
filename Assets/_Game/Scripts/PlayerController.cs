0a99a5058e762cd4e34e01677a2638f217c76978
PHoMi126 <caidinhmenh2612@gmail.com>
2023-05-19 13:54:55 +0700

    Joystick update

--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private FloatingJoystick _joystick;
+    //[SerializeField] private FixedJoystick _joystick;
     [SerializeField] private Animator _animator;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotationSpeed;
+
+    private string currentAnimName;
+
+    //private bool isGrounded = true;
+    //private bool isFall = false;
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector3(_joystick.Horizontal * _speed, _rigidbody.velocity.y, _joystick.Vertical * _speed);
+        _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed);
+    }
+
+    protected void ChangeAnimation(string animName)
+    {
+        if(currentAnimName != animName)
+        {
+            _animator.ResetTrigger(animName);
+            currentAnimName = animName;
+            _animator.SetTrigger(currentAnimName);
+        }
     }
 }
