f164bad1e5b61bc9d7e8b893835f0ba4f018f7fb
PHoMi126 <caidinhmenh2612@gmail.com>
2023-05-22 11:15:00 +0700

    update

--- Assets/_Game/Scripts/BrickController.cs
+++ Assets/_Game/Scripts/BrickController.cs
@@ -8,4 +8,26 @@ public class BrickController : MonoBehaviour
     {
         RED, BLUE, GREEN
     }
+
+    [SerializeField] MeshRenderer brickMeshRenderer;
+    [SerializeField] BrickColorScriptable colorScriptable;
+
+    public BrickType brickType = BrickType.RED;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ResetBrick(0f));
+    }
+
+    public IEnumerator ResetBrick(float timeDelay)
+    {
+        yield return new WaitForSecond(timeDelay);
+        brickMeshRenderer.gameObject.SetActive(true);
+        this.GetComponent<BoxCollider>().enabled = true;
+        brickType = (BrickType)Random.Range(0, 3);
+        brickMeshRenderer.material = colorScriptable.listMaterial[(int)brickType];
+    }
+
+
 }
